d165d2f4ec36012995cdcb5c9d1aafc204377cfe
Eloy Coto <eloy.coto@gmail.com>
2012-05-01 23:41:59 +0200

    Create new dir skeleton Create new bash aliases

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 DATE=$(date +%Y%m%d)
 DIR=$(pwd)
 
+
+#Vims conf
 if [ -d ~/.vim ]
 then
     mv ~/.vim ~/.vim-$DATE
@@ -10,9 +12,28 @@ fi
 
 if [ -f ~/.vimrc ]
 then
-    mv ~/.vimrm ~/.vim/vimrc-$DATE
+    mv ~/.vimrc ~/.vimrc-$DATE
 fi
 
 cp $DIR/vim $HOME/.vim -R
+rm  $HOME/.vimrc
 ln -s  $HOME/.vim/vimrc $HOME/.vimrc
+
+
+#BashConf
+if [ -f ~/.bashrc ]
+then
+    mv ~/.bashrc ~/.bashrc-$DATE
+fi
+cp $DIR/bash/bashrc $HOME/.bashrc
+
+
+if [ -f ~/.bash_aliases ]
+then
+    mv ~/.bash_aliases ~/.bash_aliases-$DATE
+fi
+cp $DIR/bash/bash_aliases $HOME/.bash_aliases
+
+#reload bash
+bash
 
--- update.sh
+++ update.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-DATE=$(date +%Y%m%d)
 DIR=$(pwd)
 
+#Vim Conf
 cp $DIR/vim $HOME/.vim -R
+rm  $HOME/.vimrc
 ln -s  $HOME/.vim/vimrc $HOME/.vimrc
 
+
+#Bash conf
+cp $DIR/bash/bashrc $HOME/.bashrc
+cp $DIR/bash/bash_aliases $HOME/.bash_aliases
+
+#reload bash
+bash
